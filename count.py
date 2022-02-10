b3031330c344234bfc405e7ba8d893d5ab00ac37
glen <thekalipo@gmail.com>
2022-02-10 16:39:39 +0100

    clean count.py

--- count.py
+++ count.py
@@ -18,29 +18,9 @@ DIVIDER_COLOUR = (255, 255, 0)
 BOUNDING_BOX_COLOUR = (255, 0, 0)
 CENTROID_COLOUR = (0, 0, 255)
 
-def distance(x1,x2,y1,y2):
-    return ((x1-x2)**2+(y1-y2)**2)**0.5
-
-def search_vechicle(x,y):
-    for i,v in enumerate(vehicles):
-        dist = distance(x, v[0], y, v[1])
-        if dist < 40:
-            last = vehicles[i]
-            vehicles[i] = (x,y)
-            print(f"found: ({x},{y}) index", i, "",vehicles)
-            return last
-    vehicles.append((x,y))
-    print(False)
-    return False
-
-def delVehicle(x,y):
-    for v in vehicles:
-        dist = distance(x, v[0], y, v[1])
-        if dist < 40:
-            vehicles.remove(v)
-            print("removed :", vehicles)
 frame_number = 1
 car_counter = vehicleCounter.VehicleCounter(f2.shape[:2], f2.shape[0] / 2)
+
 
 while(c.isOpened()):
     frame_number += 1
@@ -57,9 +37,6 @@ while(c.isOpened()):
 
     contours, hierarchy = cv2.findContours(dilate_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    #for i, cnt in enumerate(contours):
-    #    cv2.drawContours(f, contours, i, (0, 0, 255), 3)
-
     matches = []
     for contour in contours:
         # continue through the loop if contour area is less than 500...
@@ -75,26 +52,10 @@ while(c.isOpened()):
         cv2.rectangle(processed, (x, y), (x + w - 1, y + h - 1), BOUNDING_BOX_COLOUR, 1)
         cv2.circle(processed, centroid, 2, CENTROID_COLOUR, -1)
 
-        #processed = process_frame(frame_number, fgray, f2gray, car_counter)
-
-        """
-        if 155<=y<=165 and search_vechicle(x+w/2, y+h/2):
-            count += 1
-        elif y > 165:
-            delVehicle(x+w/2, y+h/2) # pas du tout opti mais bon ^^
-        """
-        # draw the bounding boxes
-        #cv2.rectangle(f, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
     car_counter.update_count(matches, processed)
-
-    #cv2.line(f, (0,160), (640, 160), (255,0,0), 2)
-    #cv2.putText(f,"count : "+str(car_counter.), (10,50), 0, 1, 255,2)
 
     cv2.imshow('Detected Objects', processed)
     
-
-    #imshow("diff", dilate_frame)
     k = cv2.waitKey(20)#20
 
     if k == 27:
